a5113692f5684b125a2871a1e6e541b17cb8f721
EvillDeadSpace <amartubic1@gmail.com>
2022-05-11 14:26:12 +0200

    Forma1

---
+++ Script.js
@@ -0,0 +1,13 @@
+< script type = "text/javascript" >
+    $(function() {
+
+        $('#login-show').click(function() {
+            $('#login-modal').fadeIn().css("display", "flex");
+        });
+
+        $('.close-modal').click(function() {
+            $('#login-modal').fadeOut();
+        });
+    }); <
+<
+/script>
